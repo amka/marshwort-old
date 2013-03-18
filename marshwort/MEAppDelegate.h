645a9b81f3d8929b55282dbe39c2d61067b33add
Andrey M <meamka@ya.ru>
2013-03-18 13:25:07 +0400

    Initial Commit

---
+++ marshwort/MEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEAppDelegate.h
+//  marshwort
+//
+//  Created by Andrey M. on 18.03.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ marshwort/MEAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MEAppDelegate.m
+//  marshwort
+//
+//  Created by Andrey M. on 18.03.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import "MEAppDelegate.h"
+
+@implementation MEAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
